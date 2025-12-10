bd3aa3f279e8a39a456c2cb5b113af36672bcf64
Ledent Antoine <antoineledent25@gmail.com>
2025-12-10 04:11:51 +0100

    • Création du script via Gemini
    - Version : 2
    
    <!> Erreur dans le code...

---
+++ Versions antérieures/Script (V1).ps1
@@ -0,0 +1,11 @@
+﻿$paths = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall",
+         "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+
+$paths | ForEach-Object {
+    Get-ChildItem $_ | ForEach-Object {
+        [PSCustomObject]@{
+            Nom = $_.GetValue("DisplayName")
+            Version = $_.GetValue("DisplayVersion")
+        }
+    }
+} | Where-Object { $_.Nom } | Sort-Object Nom | Format-Table -AutoSize
